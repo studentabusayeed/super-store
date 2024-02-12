8e641cfd2314ca6100aaa33a395641fb96733f1a
studentAbuSayeed <studentabusayeed2@gmail.com>
2024-02-13 00:09:03 +0600

    first commit

---
+++ src/Route/Route.jsx
@@ -0,0 +1,21 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import MainLayout from "../MainLayout/MainLayout";
+import Home from "../pages/Home/Home/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children:[
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
+    },
+]);
+
+export default router;
